a4f9348392ada8372ace59f785c342498e3bbcc2
Christian Sullivan <euforic@users.noreply.github.com>
2023-03-08 01:30:27 -0800

    fix path matching (#9)

--- todos/todos_test.go
+++ todos/todos_test.go
@@ -2,7 +2,6 @@ package todos_test
 
 import (
 	"encoding/json"
-	"os"
 	"reflect"
 	"testing"
 
@@ -25,6 +24,36 @@ func TestSearch(t *testing.T) {
 			commentType: []string{"TODO", "FIXME"},
 			want:        []todos.Comment{},
 			wantErr:     false,
+		},
+		{
+			name:        "SingleFileMatch",
+			dir:         "testdata/single-file-match",
+			ignores:     []string{".bin", "node_modules/"},
+			commentType: []string{"TODO", "FIXME"},
+			want: []todos.Comment{
+				{
+					FilePath: "testdata/single-file-match/test.go",
+					Line:     5,
+					Type:     "TODO",
+					Text:     "do something",
+					Author:   "",
+				},
+				{
+					FilePath: "testdata/single-file-match/test.go",
+					Line:     11,
+					Type:     "FIXME",
+					Text:     "do something",
+					Author:   "",
+				},
+				{
+					FilePath: "testdata/single-file-match/test.go",
+					Line:     14,
+					Type:     "TODO",
+					Text:     "do something",
+					Author:   "user",
+				},
+			},
+			wantErr: false,
 		},
 		{
 			name:        "SingleFileMatch",
@@ -93,31 +122,35 @@ func TestSearch(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name:        "MultipleFileMatchesIgnoreYAML",
+			dir:         "testdata/multiple-file-matches",
+			ignores:     []string{".bin", "*.yml"},
+			commentType: []string{"TODO", "FIXME"},
+			want: []todos.Comment{
+				{
+					FilePath: "testdata/multiple-file-matches/file1.go",
+					Line:     5,
+					Type:     "FIXME",
+					Text:     "fix this",
+					Author:   "",
+				},
+				{
+					FilePath: "testdata/multiple-file-matches/file2.go",
+					Line:     5,
+					Type:     "TODO",
+					Text:     "do something",
+					Author:   "john.doe",
+				},
+			},
+			wantErr: false,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// Create a temporary .gitignore file with the ignore rules
-			ignoreFile, err := os.CreateTemp("", ".gitignore")
-			if err != nil {
-				t.Errorf("Failed to create temporary .gitignore file: %v", err)
-				return
-			}
-			defer os.Remove(ignoreFile.Name())
-
-			for _, ignore := range tt.ignores {
-				if _, err := ignoreFile.WriteString(ignore + "\n"); err != nil {
-					t.Errorf("Failed to write ignore rule to .gitignore file: %v", err)
-					return
-				}
-			}
-			if err := ignoreFile.Close(); err != nil {
-				t.Errorf("Failed to close temporary .gitignore file: %v", err)
-				return
-			}
-
 			// Search for comments using the temporary .gitignore file
-			got, err := todos.Search(tt.dir, tt.commentType, []string{ignoreFile.Name()})
+			got, err := todos.Search(tt.dir, tt.commentType, tt.ignores)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Search() error = %v, wantErr %v", err, tt.wantErr)
 				return
